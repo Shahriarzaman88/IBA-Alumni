f503a1c3307987665c10e9ddacdbfebe208bd21b
shahriar.zaman <shahriar.zaman@randomaccessinc.com>
2022-06-14 01:58:13 -0400

    Add project files.

---
+++ IBA Alumni/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using IBA_Alumni.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBA_Alumni.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<ActionResult<ApplicationUserModel>> PostApplicationUser(ApplicationUserModel userModel)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = userModel.UserName,
+                Email = userModel.Email,
+                FullName = userModel.FullName,
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, userModel.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ IBA Alumni/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IBA_Alumni.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FullName { get; set; }
+
+    }
+}
